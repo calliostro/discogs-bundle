e18cadd4d64950d79127bb92d5f3ba5df5ac629c
calliostro <calliostro@gmail.com>
2025-08-21 12:15:45 +0200

    Update dependencies for Symfony 6.4/7 support and PHP 8.1+ compatibility

--- src/DependencyInjection/CalliostroDiscogsExtension.php
+++ src/DependencyInjection/CalliostroDiscogsExtension.php
@@ -5,7 +5,7 @@ namespace Calliostro\DiscogsBundle\DependencyInjection;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\Reference;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
 /**
